ba2788c34efe54cfe7aac4ab1fdb912925c31b09
Ranjit Shinde <ranjit.shinde.thoughtworks@aegonlife.com>
2020-01-23 22:39:28 +0530

    Renames test description

--- test/handler/handler.integration.spec.ts
+++ test/handler/handler.integration.spec.ts
@@ -11,7 +11,6 @@ const expect = chai.expect;
 
 describe("handler (Integration)", () => {
 
-    describe("associate a nudge with a user", () => {
         let apiId = '';
 
         beforeEach(async () => {
@@ -19,8 +18,9 @@ describe("handler (Integration)", () => {
             await UserFixture.deleteAll();
         });
 
-        it("should associate nudge to user on receiving policy issued event", (async function () {
+        it("should return user given user exists for a user id", (async function () {
             const userId = "user-101";
+
             await UserFixture.add(new User(userId, "Ranjit"));
 
             const apiUrl = `http://localhost:4567/restapis/${apiId}/test/_user_request_/users/${userId}`;
@@ -33,5 +33,4 @@ describe("handler (Integration)", () => {
             expect(user.userId).to.be.equal(userId);
             expect(user.firstName).to.be.equal("Ranjit");
         })).timeout(15000);
-    });
 });
